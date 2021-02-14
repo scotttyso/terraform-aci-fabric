8038d53c1cb81d429603055ca302ce2fa5516f46
scotttyso <tyscott@cisco.com>
2021-02-13 19:55:59 -0500

    fixing mistake

--- test/variables.tf
+++ test/variables.tf
@@ -1,13 +1,12 @@
 variable "aciUser" {
   description = "If using a Domain with the User Remember to add apic#[domain]\\<username>"
   type        = string
-  default     = "admin"
 }
+
 
 variable "aciPass" {
   description = "NEVER Store your USERNAME or PASSWORD in a Public Repository"
   type        = string
-  default     = "cisco123"
 }
 
 variable "aciUrl" {
